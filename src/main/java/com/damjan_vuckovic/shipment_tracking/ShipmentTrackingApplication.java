6d20d76cf95bbdca077faad9c03650f8338d56e8
user <arongy1988@gmail.com>
2026-06-20 20:55:53 +0200

    Fixed Flyway migration files, fixed typo in @Table

--- src/main/java/com/damjan_vuckovic/shipment_tracking/ShipmentTrackingApplication.java
+++ src/main/java/com/damjan_vuckovic/shipment_tracking/ShipmentTrackingApplication.java
@@ -8,8 +8,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class ShipmentTrackingApplication {
 
 	public static void main(String[] args) {
-		//System.out.println(new BCryptPasswordEncoder().encode("user"));
-		//System.out.println(new BCryptPasswordEncoder().encode("admin"));
 		SpringApplication.run(ShipmentTrackingApplication.class, args);
 	}
 
