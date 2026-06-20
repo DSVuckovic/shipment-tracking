d27da455e8717a7f090791cb98cebee595ef4959
DSVuckovic <vuckovic.s.damjan@gmail.com>
2026-06-20 08:21:26 +0200

    Initial project setup, dependencies added

---
+++ src/test/java/com/damjan_vuckovic/shipment_tracking/ShipmentTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damjan_vuckovic.shipment_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
