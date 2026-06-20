004d0c2293e9fb51d3697038414225fc2594c4a3
user <arongy1988@gmail.com>
2026-06-20 10:01:59 +0200

    Added Flyway scripts

--- src/main/java/com/damjan_vuckovic/shipment_tracking/ShipmentTrackingApplication.java
+++ src/main/java/com/damjan_vuckovic/shipment_tracking/ShipmentTrackingApplication.java
@@ -2,11 +2,14 @@ package com.damjan_vuckovic.shipment_tracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ShipmentTrackingApplication {
 
 	public static void main(String[] args) {
+		//System.out.println(new BCryptPasswordEncoder().encode("user"));
+		//System.out.println(new BCryptPasswordEncoder().encode("admin"));
 		SpringApplication.run(ShipmentTrackingApplication.class, args);
 	}
 
