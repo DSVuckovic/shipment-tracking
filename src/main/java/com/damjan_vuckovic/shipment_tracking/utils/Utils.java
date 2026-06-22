03dbce9f379ddf6504121adf1d96e7f3f81e6ca3
DSVuckovic <vuckovic.s.damjan@gmail.com>
2026-06-22 09:41:16 +0200

    Added bulk import of shipments using OpenCSV and Apache POI

--- src/main/java/com/damjan_vuckovic/shipment_tracking/utils/Utils.java
+++ src/main/java/com/damjan_vuckovic/shipment_tracking/utils/Utils.java
@@ -6,6 +6,10 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Utils {
 
     public static User getCurrentUser() {
@@ -21,4 +25,24 @@ public class Utils {
                 .getAuthorities().stream()
                 .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
     }
+
+    private static final DateTimeFormatter[] DATE_FORMATTERS = {
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"),
+            DateTimeFormatter.ISO_LOCAL_DATE_TIME
+    };
+
+    public static LocalDateTime stringToDate(String date) {
+        for (DateTimeFormatter formatter : DATE_FORMATTERS) {
+            try {
+                return LocalDateTime.parse(date, formatter);
+            }
+            catch (DateTimeParseException ignored) {
+
+            }
+        }
+
+        throw new IllegalArgumentException("Invalid date format");
+    }
+
+
 }
